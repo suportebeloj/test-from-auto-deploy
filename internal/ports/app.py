5ef1cb82b8de1a361b5ccc3ad7126468b94fd6c3
Carlos Eduardo <suportebeloj@gmail.com>
2022-10-21 10:18:16 -0300

    implement API interface

---
+++ internal/ports/app.py
@@ -0,0 +1,13 @@
+class APIInterface:
+
+    def GetAddition(self, a, b):
+        pass
+
+    def GetSubtraction(self, a, b):
+        pass
+
+    def GetMultiplication(self, a, b):
+        pass
+
+    def GetDivision(self, a, b):
+        pass
