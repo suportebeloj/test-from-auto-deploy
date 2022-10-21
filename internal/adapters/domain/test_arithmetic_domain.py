e817b7446b98145b0349121f4d66128942450e12
Carlos Eduardo <suportebeloj@gmail.com>
2022-10-21 10:57:01 -0300

    test arithmetic domain file, initial test sum and subtract

---
+++ internal/adapters/domain/test_arithmetic_domain.py
@@ -0,0 +1,19 @@
+
+
+from internal.adapters.domain.arithmetic import ArithCore
+
+
+def test_calc_sum():
+    mockData = [(1,2,3), (3,3,6), (2,2,4)]
+
+    core = ArithCore()
+    for v in mockData:
+        assert core.addition(v[0], v[1]) == v[2]
+
+
+def test_calc_sub():
+    mockData = [(1,2,-1), (3,3,0), (2,2,0), (10,3,7)]
+
+    core = ArithCore()
+    for v in mockData:
+        assert core.subtraction(v[0], v[1]) == v[2]
